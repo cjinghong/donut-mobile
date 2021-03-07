60aabd862ca7a2b94cd5dee66cf3e0a743212684
Chan Jing Hong <jinghongchan@gmail.com>
2021-03-07 23:41:12 +0800

    Able to instantiate web3

--- app/screens/wallet/wallet-screen.tsx
+++ app/screens/wallet/wallet-screen.tsx
@@ -1,18 +1,32 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { observer } from "mobx-react-lite"
 import { View } from "react-native"
 import { Screen } from "../../components"
 import { StyleService, Text, useStyleSheet } from "@ui-kitten/components"
 // import { useNavigation } from "@react-navigation/native"
 // import { useStores } from "../../models"
+import { Web3 } from "web3-react-native"
 
 export const WalletScreen = observer(function WalletScreen() {
   const styles = useStyleSheet(styleService)
+  const [web3, setWeb3] = useState(null)
+  console.log(Web3)
+  // const web3 = new Web3(
+  //   new Web3.providers.HttpProvider('https://mainnet.infura.io/')
+  // )
   // Pull in one of our MST stores
   // const { someStore, anotherStore } = useStores()
 
   // Pull in navigation via hook
   // const navigation = useNavigation()
+  useEffect(() => {
+    const initWeb3 = async () => {
+      const instance = await Web3('https://mainnet.infura.io/')
+      setWeb3(instance)
+    }
+    initWeb3()
+  }, [])
+
   return (
     <Screen style={styles.container} preset="fixed">
       <View style={styles.card}>
